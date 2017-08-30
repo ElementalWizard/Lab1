1b7df7e868b615620e778b79a81ada35ca17c4fb
Alexander <josian.velez@upr.edu>
2017-08-30 11:40:09 -0400

    Add files via upload

---
+++ src/orderedStructures/Arithmetic.java
@@ -0,0 +1,39 @@
+package orderedStructures;
+
+public class Arithmetic extends Progression {
+	private double commonDifference; 
+	
+	public Arithmetic(double firstValue, double commonDifference) { 
+		super(firstValue); 
+		this.commonDifference = commonDifference; 
+	}
+	
+	@Override
+	public double nextValue() {
+        try{
+            current = current * commonDifference;
+
+        }catch(IllegalStateException  e){
+            e.printStackTrace();
+            System.out.println("error2");
+        }
+		return current;
+	}
+	
+	public String toString(){
+		return "Ari"+"( " + super.firstValue() +", "+ this.nextValue()+ " )";
+	}
+	
+	public double getTerm(int n){
+		double temp = super.firstValue();
+		if(n==1){
+			return temp;
+		}
+		
+		for(int i =2;i<=n;i++){
+			temp=this.nextValue();
+		}
+		return temp;
+	}
+
+}
